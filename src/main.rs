bb28ba32ae70e5651ea69e5256e8d53421f7e9e9
duke <naim.giger@outlook.com>
2024-12-19 10:24:11 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub fn multiply(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiply() {
+        assert_eq!(multiply(2, 2), 4);
+        assert_eq!(multiply(-1, 1), -1);
+    }
+}
+
